841840a722ed72cc14beeb0a710fe520c25685d8
GAZi101 <gemardy@gmail.com>
2020-10-31 22:54:53 +0700

    perbaikan navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { LoginInfo } from './service/login-model';
+import { LoginService } from './service/login.service';
+import { NavItem } from './service/nav-item';
+// For MDB Angular Free
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LoginService]
 })
 export class AppComponent {
   title = 'brand-new-smartaml';
+  loginInfo: LoginInfo;
+  navItem:NavItem[];
+
+  constructor(private loginService: LoginService) {
+
+  }
+
+  ngOnInit() {
+    // this.loginService.fetchlogininfo();
+    this.loginService.getJSON().subscribe((data:LoginInfo) => {
+      this.loginInfo = data;
+      this.navItem = data.menus;
+      console.log(this.loginInfo);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginInfo } from './service/login-model';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+// import { NavbarTreeComponent } from './global-components/navbar-tree/navbar-tree.component';
+// import { NavbarModule, WavesModule, ButtonsModule } from 'angular-bootstrap-md';
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatMenuModule
+  ]
+})
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatMenuModule
   ],
-  providers: [],
+  providers: [LoginInfo],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
